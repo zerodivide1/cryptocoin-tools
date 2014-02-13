89067e3d3188f999c1dcfcdd0a039f487de38f74
Sean Payne <seantpayne@gmail.com>
2014-02-12 21:12:35 -0800

    Added script to allow waiting on a BTC transaction for a specific number of confirmations before returning.

---
+++ btc_waitfortx.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Waits until a given number of confirmations have been announced for 
+# a given transaction.
+# Usage:
+# btc_waitfortx.sh <TX_HASH> <CONFIRMATIONS>
+#
+# Dependencies:
+#	curl
+#	jq (https://github.com/stedolan/jq)
+#
+# Author: Sean Payne (https://github.com/zerodivide1)
+# Donate: 1GHPpkFZg8U9sVLjGASqDd8YqTHoVDVFgn
+
+TX_HASH=$1
+TX_NUM=$2
+
+API=https://blockchain.info
+
+getConfirmations () {
+	local api_result=$(curl -s "$API/rawtx/$1" | jq '.block_height')
+	if [[ "$api_result" == "null" ]] ; then
+		echo 0
+	else
+		local blockheight=$(curl -s "$API/latestblock" | jq '.height')
+		local confirmations=$(expr $blockheight - $api_result + 1)
+		echo $confirmations
+	fi
+}
+
+CONFIRMS=0
+while [ $CONFIRMS -lt $TX_NUM ]; do
+	CONFIRMS=$(getConfirmations "$TX_HASH")
+	echo "Confirmations: $CONFIRMS" 1>&2
+	if [ $CONFIRMS -lt $TX_NUM ]; then
+		sleep 15
+	fi
+done
+
+exit 0
